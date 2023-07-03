4ee9102170e29604c82dcb7c9ed83e95327a366e
Joshua Nauman <13076588+TheeNawMan@users.noreply.github.com>
2023-07-03 14:15:24 -0500

    Initial working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # goHTTP
  A Simple goHTTP server with download/upload funcionality for use during pentesting
+
+ Has simple website for downloading files.
+
+ # TODO
+ - Write logic for saving files from upload
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"io"
+)
+
+type PageData struct {
+	Title string
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/download/", downloadHandler)
+	http.HandleFunc("/upload", uploadHandler)
+
+	log.Println("Server started on port 9000")
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	data := PageData{
+		Title: "Home",
+	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := r.URL.Path[len("/download/"):]
+	filePath = filepath.Join("download", filePath) // Assuming the files are stored in the "download" folder
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileInfo.Name()))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Failed to retrieve file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// TODO: Save the uploaded file
+	
+	fmt.Printf("Received file: %s, Size: %d bytes\n", handler.Filename, handler.Size)
+
+	fmt.Fprintf(w, "Coming Later")
+}
